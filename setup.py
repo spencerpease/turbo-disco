e811281dc23a6cc82c91a5c6b512e3ad7f252f41
Spencer Pease <spencer.p.816@gmail.com>
2019-12-10 14:26:27 -0800

    Setup package

---
+++ setup.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+
+    base_dir = Path(__file__).parent
+    src_dir = base_dir / 'src'
+
+    about = {}
+    with (src_dir / "special_package" / "__about__.py").open() as f:
+        exec(f.read(), about)
+
+    with (base_dir / "README.rst").open() as f:
+        long_description = f.read()
+
+    install_requirements = [
+
+    ]
+
+    interactive_requirements = [
+
+    ]
+
+    test_requirements = [
+
+    ]
+
+    doc_requirements = [
+
+    ]
+
+    setup(
+        name=about['__title__'],
+        version=about['__version__'],
+
+        description=about['__summary__'],
+        long_description=long_description,
+        url=about["__uri__"],
+
+        author=about["__author__"],
+        author_email=about["__email__"],
+
+        # Advertsing package to people
+        classifiers=[
+
+        ],
+
+        # Look inside source directory for package, instead of same name in root dir
+        package_dir={'': 'src'},
+        packages=find_packages(where='src'),
+
+        include_package_data=True,
+
+        install_requires=install_requirements,
+        tests_require=test_requirements,
+        extras_require={
+            'docs': doc_requirements,
+            'test': test_requirements,
+            'interactive': interactive_requirements,
+            'dev': doc_requirements + test_requirements + interactive_requirements,
+        },
+
+        # This sets up applications
+        #entry_points="""
+        #        [console_scripts]
+        #        simulate=vivarium.interface.cli:simulate
+        #    """,
+
+        zip_safe=False,
+    )
---
+++ src/turbo-disco/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "special_package"
+__summary__ = "Learning how to build pyhon packages"
+__uri__ = ""
+
+__version__ = "1.0.0"
+
+__author__ = "Spencer Pease"
+__email__ = "spease@uw.edu"
+
+# __license__ = "GNU GPLv3"
+# __copyright__ = f"Copyright 2016-2019 {__author__}"
